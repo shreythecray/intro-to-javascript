872fb12baf454c285b4f7767c132b595a5bbe9ab
Shreya <shrgupta@ucdavis.edu>
2021-03-06 18:23:08 -0800

    split helloworld into 4 separate steps

--- .bit/tests/test1.helloworld.js
+++ .bit/tests/test1.helloworld.js
@@ -9,7 +9,7 @@ let helloworld = hello()
 let test_output = "Hello World!"
 
 if(helloworld != test_output){
-    throw new Error(`You're not fizzing well - Zan - Got: ${helloworld}, was expecting: ${test_output}.`)
+    throw new Error(`You got ${helloworld}, was expecting: ${test_output}.`)
 }
 
 console.log("Yay! 🎉🎉🎉🎉🎉🎉🎉🍾")
